c86b264990de50c1c50e175b77ae5361eda82da5
Sarah Harvey <s@shh.sh>
2020-01-01 22:42:33 -0800

    update blah

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
 from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfpage import PDFTextExtractionNotAllowed
 from pdfminer.pdfparser import PDFParser
 
-from paperminer.converter import TestConverter
+from paperminer.converter import PaperToTextConverter
 
 # Open a PDF file.
-fp = open('samples/test1.pdf', 'rb')
+fp = open('sample.pdf', 'rb')
 # Create a PDF parser object associated with the file object.
 parser = PDFParser(fp)
 # Create a PDF document object that stores the document structure.
@@ -17,14 +14,6 @@ document = PDFDocument(parser)
 # Check if the document allows text extraction. If not, abort.
 if not document.is_extractable:
     raise PDFTextExtractionNotAllowed
-# Create a PDF resource manager object that stores shared resources.
-rsrcmgr = PDFResourceManager()
-# Create a PDF device object.
-device = TestConverter(rsrcmgr=rsrcmgr)
-# Create a PDF interpreter object.
-interpreter = PDFPageInterpreter(rsrcmgr, device)
-# Process each page contained in the document.
-for page in PDFPage.create_pages(document):
-    interpreter.process_page(page)
-    # receive the LTPage object for the page.
-    # layout = device.get_result()
+# Create a PDF converter
+converter = PaperToTextConverter(document=document)
+converter.get_result()
