741d6d1dbb9d86b1d826576ab58f9bf74cbd21cc
Sarah Harvey <s@shh.sh>
2020-01-01 23:38:27 -0800

    Fix Travis CI and also clean up travis job names

--- uwsgi.py
+++ uwsgi.py
@@ -1,4 +1,6 @@
 import os
+from typing import Any, cast
+
 from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='webapp/build')
@@ -7,8 +9,8 @@ app = Flask(__name__, static_folder='webapp/build')
 # Serve React App
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
+def serve(path: str) -> Any:
+    if path != "" and os.path.exists(cast(str, app.static_folder) + '/' + path):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
