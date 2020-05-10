6b79bb7d811093832dbf6cc805fadd96753b2e71
Sarah Harvey <s@shh.sh>
2020-05-10 00:53:36 -0700

    Add title tag

--- webapp/src/Home.js
+++ webapp/src/Home.js
@@ -1,10 +1,14 @@
 
 import React, { Component } from 'react';
+import {Helmet} from "react-helmet";
 
 class Home extends Component {
     render() {
         return (
             <div>
+                <Helmet>
+                    <title>monograph</title>
+                </Helmet>
             </div>
         );
     }
