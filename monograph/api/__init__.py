2c4a9d426c1620f90bb312e3b738671018c4b89a
Emil Harvey <harvey.emilh@gmail.com>
2020-06-11 11:33:27 -0400

    init

--- monograph/api/__init__.py
+++ monograph/api/__init__.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 from monograph.api.analyze import AnalyzeAPI
 from monograph.api.library import LibraryAPI
 from monograph.api.search import SearchAPI
+from monograph.api.login import LoginAPI
 
 
 class ApiBlueprint(Blueprint):
@@ -13,3 +14,4 @@ class ApiBlueprint(Blueprint):
         self.api.add_resource(AnalyzeAPI, '/analyze')
         self.api.add_resource(SearchAPI, '/search')
         self.api.add_resource(LibraryAPI, '/library', '/library/<id>')
+        self.api.add_resource(LoginAPI, '/login')
